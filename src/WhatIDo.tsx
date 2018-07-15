f8e1dc72041f1c1c599c1df3ad5899876654acac
Lucia Velasco <luciakvelasco@gmail.com>
2018-07-15 17:47:59 +0100

    Adds beginning of next section on page

---
+++ src/WhatIDo.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+import { Component } from 'react'
+import './landingSection.css'
+
+export class LandingSection extends Component {
+  public render() {
+    return (
+      <section className="what-i-do">
+        {/* use css-grid here */}
+        <div className="how-do">
+          {/* icon 1 */}
+          {/* icon description 1 */}
+          {/* icon 2 */}
+          {/* description 2 */}
+        </div>
+        <div className="why">
+          {/* icon 1 */}
+          {/* icon description 1 */}
+          {/* icon 2 */}
+          {/* description 2 */}
+        </div>
+      </section>
+    )
+  }
+}
