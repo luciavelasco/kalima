1796ccb98209d65479b581ef794affdb211705b3
Lucia Velasco <luciakvelasco@gmail.com>
2018-07-15 11:36:23 +0100

    Removes unused html

--- src/App.tsx
+++ src/App.tsx
@@ -18,13 +18,6 @@ class App extends React.Component {
           <div className="logo-line"></div>
           <div className="name">KALIMA</div>
         </div>
-        {/*<header className="App-header">*/}
-          {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          {/*<h1 className="App-title">Welcome to React</h1>*/}
-        {/*</header>*/}
-        {/*<p className="App-intro">*/}
-          {/*To get started, edit <code>src/App.tsx</code> and save to reload.*/}
-        {/*</p>*/}
       </div>
     );
   }
