64cd97cbbf1a42bdb4fe15a16c5f4ce41dbd3d89
Lucia Velasco <luciakvelasco@gmail.com>
2018-07-15 11:40:59 +0100

    Fixes images import, self-closes html

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import * as React from 'react';
 import './App.css';
 
-// import * as logo from './logo.svg';
 // import * as portrait from '../public/portrait_goodwood.jpg';
 // import * as fullBody from '../public/rome_balcony.jpg';
-import * as sand from '../public/sand.png';
+import * as sand from './images/sand.png';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
         <div className="banner">
-          <div className="sand"></div>
-          <img className="sand" src={sand}></img>
-          <div className="building"></div>
-          <div className="happy-worker"></div>
-          <div className="logo-line"></div>
+          <div className="sand"/>
+          <img className="sand" src={sand}/>
+          <div className="building"/>
+          <div className="happy-worker"/>
+          <div className="logo-line"/>
           <div className="name">KALIMA</div>
         </div>
       </div>
